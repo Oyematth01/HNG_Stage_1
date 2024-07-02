49b8cf36b82772a56ef981907cf1a0a005bd4285
Oyematth01 <oyematth@gmail.com>
2024-07-02 13:31:44 +0100

    stage1

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTimeAndDay() {
+    const now = new Date();
+    const utcTime = now.toUTCString().split(' ')[4];
+    const day = now.toLocaleString('en-US', { weekday: 'long' });
+
+    document.querySelector('[data-testid="currentTimeUTC"]').textContent = utcTime;
+    document.querySelector('[data-testid="currentDay"]').textContent = day;
+}
+
+updateTimeAndDay();
+setInterval(updateTimeAndDay, 1000);
